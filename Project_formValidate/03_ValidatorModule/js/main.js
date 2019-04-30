3171a6e47bd01b6a8e1a8b098b022762038d9860
Jasonlee <690163223@qq.com>
2019-04-30 20:12:35 +0800

    project

---
+++ Project_formValidate/03_ValidatorModule/js/main.js
@@ -0,0 +1,6 @@
+//选中页面中所有的自定义属性 【data-rule】
+//解析自定义属性
+//验证
+
+var validator = new Validator(user_input,rule);
+validator.is_valid();
---
+++ Project_formValidate/03_ValidatorModule/js/validator.js
@@ -0,0 +1,20 @@
+//自定义验证器
+//大致的一个框架
+$(function(){//闭包防止污染全局变量
+    'use strict';
+    window.Validator = function(val,rule){
+        //总的验证方法，最后会提交是true还是false
+        this.is_valid = function(){
+            return ;
+        };
+        this.validator_max = function(){
+            
+        };
+        this.validator_xxxxx = function(){
+
+        }
+        //..
+        //..
+        //..
+    }
+})
