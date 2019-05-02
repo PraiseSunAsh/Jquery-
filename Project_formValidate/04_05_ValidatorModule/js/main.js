a68c50b56fa54e3bcbfefa77c5a3a8737ae5a27b
Jasonlee <690163223@qq.com>
2019-05-02 09:37:52 +0800

    project

--- Project_formValidate/04_05_ValidatorModule/js/main.js
+++ Project_formValidate/04_05_ValidatorModule/js/main.js
@@ -6,11 +6,13 @@
 //这时里面的Validator构造函数就会显示未定义（因为Validator也被Jquery闭包包起来了）
 jQuery(function () {
     //创建一个新的验证对象，并在构造器中创建规则
-    var validator = new Validator('abcdeffa',{
+    var validator = new Validator(' 1 ',{
         maxLength:5,
     });
     // var res = validator.validate_min();
     // console.log(res);
-    var res = validator.validate_maxLength();
+    // var res = validator.validate_maxLength();
+    // console.log(res);
+    var res = validator.validate_nullable();
     console.log(res);
 });
